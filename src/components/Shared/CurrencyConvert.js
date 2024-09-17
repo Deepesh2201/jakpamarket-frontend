2aeb8257cd050bf64dc95abecd63befbbe67ed3f
Deepesh Raj <deepeshinfo22@gmail.com>
2024-09-17 23:05:09 +0530

    init

---
+++ src/components/Shared/CurrencyConvert.js
@@ -0,0 +1,23 @@
+function CurrencyConvert({price}) {
+    if(price){
+        const priceTypeConst = parseFloat(price).toFixed(2);
+        if(typeof window !== 'undefined'){
+            if(localStorage.getItem("shopoDefaultCurrency")){
+                const getDefaultCurrency=JSON.parse(localStorage.getItem("shopoDefaultCurrency"));
+                const priceConverted = priceTypeConst * parseFloat(getDefaultCurrency.currency_rate).toFixed(2);
+                if(getDefaultCurrency.currency_position==='left'){
+                    return `${getDefaultCurrency.currency_icon}${priceConverted.toFixed(2)}`;
+                }else{
+                return `${priceConverted.toFixed(2)}${getDefaultCurrency.currency_icon}`;
+                }
+            }
+            return priceTypeConst
+        }else{
+            return priceTypeConst;
+        }
+    }else{
+        return '';
+    }
+}
+
+export default CurrencyConvert;
