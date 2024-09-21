5bfddb3c9d7638a58acee00a0f1211d37f72005c
Deepesh Raj <deepeshinfo22@gmail.com>
2024-09-21 22:37:56 +0530

    updated

--- src/components/Auth/Signup/SignupWidget.jsx
+++ src/components/Auth/Signup/SignupWidget.jsx
@@ -16,7 +16,7 @@ function SignupWidget({ redirect = true, signupActionPopup,changeContent }) {
   const [fname, setFname] = useState("");
   const [lname, setLname] = useState("");
   const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("+880");
+  const [phone, setPhone] = useState("+233");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [checked, setCheck] = useState(false);
@@ -187,7 +187,7 @@ function SignupWidget({ redirect = true, signupActionPopup,changeContent }) {
             )}
           </div>
           {parseInt(phone_number_required)===1&&(
-              <div className="input-item mb-5 relative">
+              <div className="input-item mb-5">
                 <InputCom
                     placeholder={ServeLangItem()?.Phone_Number}
                     label={ServeLangItem()?.phone + "*"}
@@ -203,30 +203,7 @@ function SignupWidget({ redirect = true, signupActionPopup,changeContent }) {
                 ) : (
                     ""
                 )}
-                <button onClick={()=>setCountryDropToggle(!countryDropToggle)} type="button" className="w-[70px] h-[50px] bg-qgray-border absolute left-0 top-[29px] flex justify-center items-center">
-                  <div className="flex items-center">
-                  <span>
-                    <Image width="30" height="20" src={`/assets/images/countries/${selectedCountry}.svg`} alt="country"/>
-                  </span>
-                    <span className="text-qgray">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 14l-4-4h8z"/></svg>
-                  </span>
-                  </div>
-                </button>
-                <div style={{boxShadow: "rgb(0 0 0 / 14%) 0px 15px 50px 0px",display:countryDropToggle?'block':'none'}} className="country-dropdown-list w-[250px] h-[250px] bg-white absolute left-0 top-[80px] z-20 overflow-y-scroll">
-                  <ul>
-                    {getCountries && getCountries.length>0&&getCountries.map((item,i)=>(
-                        <li onClick={()=>selectCountryhandler(item)} key={i} className="flex space-x-1.5 items-center px-3 py-1 cursor-pointer">
-                        <span className="w-[25px]">
-                           <Image width="25" height="15" src={`/assets/images/countries/${item.code}.svg`} alt="country"/>
-                        </span>
-                          <span className="text-sm text-qgray capitalize flex-1">
-                          {item.name}
-                        </span>
-                        </li>
-                    ))}
-                  </ul>
-                </div>
+                
               </div>
           )}
           <div className="flex sm:flex-row flex-col space-y-5 sm:space-y-0 sm:space-x-5 rtl:space-x-reverse mb-5">
